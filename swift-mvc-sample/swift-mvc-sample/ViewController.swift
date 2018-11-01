06aecb2d7c201d5e977aa046766ec0e61bdd93ac
MasamiMishima <mishima.masami@willgate.co.jp>
2018-11-01 19:49:20 +0900

    init

---
+++ swift-mvc-sample/swift-mvc-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-mvc-sample
+//
+//  Created by 三島 正三 on 2018/11/01.
+//  Copyright © 2018 三島 正三. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
